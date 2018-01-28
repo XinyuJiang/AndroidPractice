d30f28d867f3920c7c2d49483804109355fabd69
XinyuJiang <503273921@qq.com>
2018-01-28 15:52:17 +0800

    UI Practice 1.3<Until EditText)

---
+++ UIWidgetTest/app/src/main/java/com/xinyujiang/uiwidgettest/MainActivity.java
@@ -0,0 +1,35 @@
+package com.xinyujiang.uiwidgettest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.edit_text);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.button:
+                        String inputText = editText.getText().toString();
+                        Toast.makeText(MainActivity.this,inputText,
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+    }
+}
