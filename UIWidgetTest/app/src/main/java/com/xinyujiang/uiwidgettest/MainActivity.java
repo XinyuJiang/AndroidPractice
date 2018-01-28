391e9afceb416dd5142a878ba526063e81eb32c9
XinyuJiang <503273921@qq.com>
2018-01-28 16:07:21 +0800

    UI Practice 1.4<ImageView)

--- UIWidgetTest/app/src/main/java/com/xinyujiang/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/xinyujiang/uiwidgettest/MainActivity.java
@@ -5,23 +5,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editText;
 
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
         editText = (EditText) findViewById(R.id.edit_text);
+        imageView = (ImageView) findViewById(R.id.image_view);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 switch (v.getId()){
                     case R.id.button:
+                        imageView.setImageResource(R.drawable.img_2);
                         String inputText = editText.getText().toString();
                         Toast.makeText(MainActivity.this,inputText,
                                 Toast.LENGTH_SHORT).show();
