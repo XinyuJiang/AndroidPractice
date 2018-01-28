3e1760fc39956fac439ba0ac72eccb2653c75816
XinyuJiang <503273921@qq.com>
2018-01-28 16:26:53 +0800

    UI Practice 1.5<ProgressBar)

--- UIWidgetTest/app/src/main/java/com/xinyujiang/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/xinyujiang/uiwidgettest/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText editText;
 
     private ImageView imageView;
+
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,16 +23,27 @@ public class MainActivity extends AppCompatActivity {
         Button button = (Button) findViewById(R.id.button);
         editText = (EditText) findViewById(R.id.edit_text);
         imageView = (ImageView) findViewById(R.id.image_view);
+        progressBar = (ProgressBar) findViewById(R.id.progree_bar);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 switch (v.getId()){
                     case R.id.button:
-                        imageView.setImageResource(R.drawable.img_2);
-                        String inputText = editText.getText().toString();
-                        Toast.makeText(MainActivity.this,inputText,
-                                Toast.LENGTH_SHORT).show();
+                        int progress = progressBar.getProgress();
+                        progress = progress + 10;
+                        progressBar.setProgress(progress);
                         break;
+                        /*
+                        * android的控件都有一个可见属性。可以通过android:visibility进行指定，有visible,invisible和gone三种可选值。
+                        * visible表示控件是可见的，这个值是默认值
+                        * invisible表示控件不可见，但是它仍然占据着原来的位置和大小，可以理解成控件变成透明状态了
+                        * gone表示控件不仅不可见，客人切不再占用任何屏幕空间
+                        * */
+                        /*if(progressBar.getVisibility() == View.GONE) {
+                            progressBar.setVisibility(View.VISIBLE);
+                        }else {
+                            progressBar.setVisibility(View.GONE);
+                        }break;*/
                     default:
                         break;
                 }
