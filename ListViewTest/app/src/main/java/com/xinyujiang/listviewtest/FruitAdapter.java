51eafd987d9c283a6580dc24b671831ead45703d
XinyuJiang <503273921@qq.com>
2018-01-31 18:35:53 +0800

    ListView Practice 1.2 <optimize>

--- ListViewTest/app/src/main/java/com/xinyujiang/listviewtest/FruitAdapter.java
+++ ListViewTest/app/src/main/java/com/xinyujiang/listviewtest/FruitAdapter.java
@@ -13,7 +13,7 @@ import android.widget.TextView;
 import java.util.List;
 
 /**
- * Created by apple on 2018/1/31.
+ * Created by XinyuJiang on 2018/1/31.
  */
 
 public class FruitAdapter extends ArrayAdapter<Fruit>{
@@ -25,15 +25,33 @@ public class FruitAdapter extends ArrayAdapter<Fruit>{
         super(context, textViewResourceId, objects);
         resourceId = textViewResourceId;
     }
-
+/*
+* 做出两点优化：
+* 利用convertview的缓存功能使得不用每次都重新加载view；
+* 使用内部类ViewHolder对控件的实例进行缓存。使得不用每次都找Id
+* */
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View convertView, ViewGroup parent) {//convert可以将之前加载好的布局缓存，以便之后可以重用
         Fruit fruit = getItem(position);//获取当前项的Fruit实例
-        View view = LayoutInflater.from(getContext()).inflate(resourceId,parent,false);//为子项加载我们传入的布局；第三个参数为false表示只让我们在父布局中声明的Layout有效但不会为这个view添加父布局
-        ImageView fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
-        TextView fruitName = (TextView) view.findViewById(R.id.fruit_name);
-        fruitImage.setImageResource(fruit.getImageId());
-        fruitName.setText(fruit.getName());
+        View view;
+        ViewHolder viewHolder;
+        if (convertView == null){
+            view = LayoutInflater.from(getContext()).inflate(resourceId,parent,false);//为子项加载我们传入的布局；第三个参数为false表示只让我们在父布局中声明的Layout有效但不会为这个view添加父布局
+            viewHolder = new ViewHolder();
+            viewHolder.fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+            viewHolder.fruitName = (TextView) view.findViewById(R.id.fruit_name);
+            view.setTag(viewHolder);//利用settag()将viewHolder存储在view中(一个view有一个viewHolder)
+        }else {
+            view = convertView;//重用之前缓存好的布局
+            viewHolder = (ViewHolder) view.getTag();//取出viewHolder
+        }
+        viewHolder.fruitImage.setImageResource(fruit.getImageId());
+        viewHolder.fruitName.setText(fruit.getName());
         return view;
+    }
+    class ViewHolder {
+        ImageView fruitImage;
+
+        TextView fruitName;
     }
 }
