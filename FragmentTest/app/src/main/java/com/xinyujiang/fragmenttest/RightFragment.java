a8cc6476445b14a243232758f6ddd33d8d953d61
XinyuJiang <503273921@qq.com>
2018-02-02 15:09:23 +0800

    Fragment Practice 1.0

---
+++ FragmentTest/app/src/main/java/com/xinyujiang/fragmenttest/RightFragment.java
@@ -0,0 +1,21 @@
+package com.xinyujiang.fragmenttest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by XinyuJiang on 2018/2/2.
+ */
+
+public class RightFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.right_fragment,container,false);
+        return view;
+    }
+}
