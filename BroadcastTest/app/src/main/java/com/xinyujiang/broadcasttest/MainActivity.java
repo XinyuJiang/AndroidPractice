ca98f927860f65ea0362a5334b05353839fea683
XinyuJiang <503273921@qq.com>
2018-02-04 23:09:27 +0800

    Broadcast Practice 1.0 <Dynamic Register>

---
+++ BroadcastTest/app/src/main/java/com/xinyujiang/broadcasttest/MainActivity.java
@@ -0,0 +1,48 @@
+package com.xinyujiang.broadcasttest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private IntentFilter intentFilter;
+
+    private NetworkChangeReceiver networkChangeReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");//我们的广播接收器想要监听什么广播，就在这里添加相应的action
+        networkChangeReceiver = new NetworkChangeReceiver();
+        registerReceiver(networkChangeReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {//动态注册的广播接收器一定要取消注册
+        super.onDestroy();
+        unregisterReceiver(networkChangeReceiver);
+    }
+
+    class NetworkChangeReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {//NetworkChangeReceiver继承自BroadcastReceiver，这样每当网络状态改变的时候onReceive()方法就会得到执行
+            ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);//一个专门用来管理网络连接的系统服务类
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();//调用connectivityManager的getActiveNetworkInfo得到NetworkInfo实例
+            if(networkInfo != null && networkInfo.isAvailable()){
+                Toast.makeText(context,"network is available", Toast.LENGTH_SHORT).show();
+            }else{
+            Toast.makeText(context, "network is unavailable", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
