e7752c509b6efa1488357bbee567d6dd4b224337
XinyuJiang <503273921@qq.com>
2018-02-04 23:36:14 +0800

    Broadcast Practice 1.2 <Standard Broadcast>

--- BroadcastTest/app/src/main/java/com/xinyujiang/broadcasttest/MainActivity.java
+++ BroadcastTest/app/src/main/java/com/xinyujiang/broadcasttest/MainActivity.java
@@ -8,6 +8,8 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.xinyujiang.broadcasttest.MY_BROADCAST");
+                sendBroadcast(intent);
+            }
+        });
         intentFilter = new IntentFilter();
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");//我们的广播接收器想要监听什么广播，就在这里添加相应的action
         networkChangeReceiver = new NetworkChangeReceiver();
