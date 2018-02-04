f0fce45497eb65f1102109f5eef1687c7c0f73df
XinyuJiang <503273921@qq.com>
2018-02-05 00:04:15 +0800

    Broadcast Practice 1.3 <Order Broadcast>

---
+++ BroadcastTest2/app/src/main/java/com/xinyujiang/broadcasttest2/AnoeherBroadcastReceiver.java
@@ -0,0 +1,18 @@
+package com.xinyujiang.broadcasttest2;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by apple on 2018/2/4.
+ */
+
+public class AnoeherBroadcastReceiver extends BroadcastReceiver{
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context,"received in AnotherBroadcastReceiver",Toast.LENGTH_SHORT).show();
+    }
+}
