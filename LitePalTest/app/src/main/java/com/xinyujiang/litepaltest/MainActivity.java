d7887c3866917c9a0dd81df1695a1fe730323b75
XinyuJiang <503273921@qq.com>
2018-02-18 16:35:30 +0800

    LitePalTest Practice 1.0

---
+++ LitePalTest/app/src/main/java/com/xinyujiang/litepaltest/MainActivity.java
@@ -0,0 +1,37 @@
+package com.xinyujiang.litepaltest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.litepal.LitePal;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button createDatabase = (Button) findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                LitePal.getDatabase();
+            }
+        });
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Book book = new Book();
+                book.setName("The Da Vinci Code");
+                book.setAuthor("Dan Brown");
+                book.setPages(454);
+                book.setPrice(16.96);
+                book.setPress("Unknow");
+                book.save();//save()继承自DataSupport类
+            }
+        });
+    }
+}
