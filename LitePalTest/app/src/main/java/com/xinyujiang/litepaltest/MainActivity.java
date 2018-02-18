168c9279b1966b616abb5b9603fdb20fd68c7e24
XinyuJiang <503273921@qq.com>
2018-02-18 16:45:27 +0800

    LitePalTest Practice 1.1 update

--- LitePalTest/app/src/main/java/com/xinyujiang/litepaltest/MainActivity.java
+++ LitePalTest/app/src/main/java/com/xinyujiang/litepaltest/MainActivity.java
@@ -33,5 +33,31 @@ public class MainActivity extends AppCompatActivity {
                 book.save();//save()继承自DataSupport类
             }
         });
+        Button updateData = (Button) findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //update方法1，但是只能对已存储的对象进行操作，限制性比较大
+                /*Book book = new Book();
+                book.setName("The Lost Symbol");
+                book.setAuthor("Dan Brown");
+                book.setPages(510);
+                book.setPrice(19.95);
+                book.setPress("Unknow");
+                book.save();//save()继承自DataSupport类
+                book.setPrice(10.99);
+                book.save();*/
+                //update方法2
+                Book book = new Book();
+                book.setPrice(14.95);
+                book.setPress("Anchor");
+                book.updateAll("name = ? and author = ?","The Lost Symbol","Dan Brown");//相当于SQL中where的用法
+                /*如果想要update到初始值那么必须要使用setToDefault()
+                Boook book = new Book();
+                book.setToDefault("pages");
+                book.updateAll();
+                */
+            }
+        });
     }
 }
